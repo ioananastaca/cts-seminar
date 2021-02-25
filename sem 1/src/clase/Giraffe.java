f9e79863a2f216c4d49a013793e411db9821a87f
Ioana <Ioana@DESKTOP-74A3ID4>
2021-02-25 17:18:11 +0200

    adaugarea claselor pt gradina zoo

---
+++ sem 1/src/clase/Giraffe.java
@@ -0,0 +1,11 @@
+package clase;
+
+public class Giraffe extends Animal {
+
+	public Giraffe(String name) {
+		super(name);
+		
+	}
+	
+
+}
--- sem 1/src/seminar1/Main.java
+++ sem 1/src/seminar1/Main.java
@@ -1,10 +1,18 @@
 package seminar1;
 
+import clase.Giraffe;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Zoo z=new Zoo();
+		Giraffe giraffe=new Giraffe("Girafa1");
+		Giraffe giraffe2=new Giraffe("Girafa2");
+		z.addAnimal(giraffe);
+		z.addAnimal(giraffe2);
+		
+		z.feedAllAnimals();
 	}
 
 }
