15b14c285258e0b12bf875391d5dfb807b45810a
Ioana <Ioana@DESKTOP-74A3ID4>
2021-02-25 17:18:50 +0200

    adaugare clase

--- sem 1/src/clase/Zoo.java
+++ sem 1/src/clase/Zoo.java
@@ -1,5 +1,31 @@
 package clase;
 
-public class Zoo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Zoo {
+	private Zookeeper ingrijitor;
+	private List<Animal> animale;
+	public Zoo(Zookeeper ingrijitor, List<Animal> animale) {
+		super();
+		this.ingrijitor = ingrijitor;
+		this.animale = animale;
+	}
+	
+	public Zoo() {
+		super();
+		this.ingrijitor=new Zookeeper("GIGI");
+		this.animale=new ArrayList<>();
+		
+	}
+	public void addAnimal(Animal a) {
+		animale.add(a);
+	}
+	public void feedAllAnimals() {
+		for(Animal a:animale) {
+			ingrijitor.feed(a);
+		}
+		
+	}
+	
 }
--- sem 1/src/clase/Zookeeper.java
+++ sem 1/src/clase/Zookeeper.java
@@ -1,5 +1,22 @@
 package clase;
 
 public class Zookeeper {
+ private String nune;
 
+public String getNune() {
+	return nune;
+}
+
+public void setNune(String nune) {
+	this.nune = nune;
+}
+
+public Zookeeper(String nune) {
+	super();
+	this.nune = nune;
+}
+ 
+ public void feed(Animal a) {
+	 System.out.println(nune+" feeds the animal "+a.getName());
+ }
 }
