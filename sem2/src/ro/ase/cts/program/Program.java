b5f8be24d12f3def38c4e2b2c9b4863ede2f1328
Ioana <Ioana@DESKTOP-74A3ID4>
2021-03-04 16:28:52 +0200

    readers package- commit 2

--- sem2/src/ro/ase/cts/program/Program.java
+++ sem2/src/ro/ase/cts/program/Program.java
@@ -5,15 +5,16 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");//modificam doar aici acum
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
