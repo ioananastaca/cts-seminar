35fb55d637f71fe363988d6d9e485c187a6f9c1f
Ioana <Ioana@192.168.1.7>
2021-04-01 16:23:29 +0300

    initial commit

---
+++ sem6/src/ro/ase/cts/program/Main.java
@@ -0,0 +1,33 @@
+package ro.ase.cts.program;
+
+import ro.ase.cts.builder.clase.Rezervare;
+import ro.ase.cts.builder.clase.RezervareBuilder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rezervare rezervare1=new RezervareBuilder(10)
+				.setAreBauturaInclusa(true)
+				.build();
+		
+		Rezervare rezervare2=new RezervareBuilder(20)
+				.setAreBauturaInclusa(true)
+				.setGenMuzica("Pop")
+				.build();
+		
+		Rezervare rezervare3=new Rezervare(false,true,false,false,null,70);
+		//shallow copy
+		RezervareBuilder newBuilder=new RezervareBuilder().setAreMancareInclusa(true).setAreBauturaInclusa(true).setAreScaunErgonomic(false);
+		Rezervare rezervare4=newBuilder.setCodRezervare(80).build();
+		Rezervare rezervare5=newBuilder.setCodRezervare(81).build();
+	
+		System.out.println(rezervare1);
+		System.out.println(rezervare2);
+		System.out.println(rezervare3);
+		
+		
+		
+	}
+
+}
